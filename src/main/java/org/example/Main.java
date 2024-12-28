11c24a7b3f9ab79fd6af746af1717be553343397
Belal30311 <011022bn@gmail.com>
2024-12-28 13:38:32 +0200

    The second task in the first week with CodeAlpha internship

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String args[])
+    {
+        JFrame f =new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        f.setLayout(null);
+        f.getContentPane().setBackground(Color.GREEN);
+        f.setSize(600,600);
+        JLabel l=new JLabel("This is My page");
+        l.setBounds(250,300,150,70);
+        f.add(l);
+        JTextArea A =new JTextArea();
+        A.setBounds(50,50,300,100);
+        f.add(A);
+        JButton b =new JButton("Press the button");
+        b.setBounds(20,250,150,30);
+        f.add(b);
+        JLabel result = new JLabel("The Result will appear here");
+        result.setBounds(20,300,300,50);
+        f.add(result);
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = A.getText().trim(); //delete the spaces from the start and the end
+                if(text.isEmpty())
+                    result.setText("There is not found any word");
+                else
+                {
+                    String [] words = text.split("\\s+"); //fetch the words only
+                    result.setText("The number of words is: "+words.length);
+                }
+            }
+        });
+    }
+}
